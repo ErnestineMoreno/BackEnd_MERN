d64961883d37569ce0ef414f30766bb9ab4c0c78
kristenmoran <kristen.moran30@gmail.com>
2020-05-15 12:27:38 -0400

    seeded the database and created schema

---
+++ models/Job.js
@@ -0,0 +1,16 @@
+const mongoose = require('../db/connection');
+
+const jobSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: String,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('Job', jobSchema);
