fc59239221d7482604fba2db6f8beaadea47b6fa
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-05-15 10:11:26 -0700

    error handling

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,42 +1,70 @@
-const express = require('express');
-const Job = require('../models/Job');
+const express = require('express')
+const Job = require('../models/Job')
 
-const router = express.Router();
-
-
+const router = express.Router()
 
 // INDEX
 // GET api/jobs
-router.get('/', (req, res) => {
-   Job.find().then((jobs) => res.json(jobs));
-});
+router.get('/', (req, res, next) => {
+  // Use our Job model to find all of the documents
+  // in the jobs collection
+  // Then send all of the jobs back as json
+  Job.find()
+    .then((jobs) => res.json(jobs))
+    .catch(next)
+})
 
 // SHOW
 // GET api/jobs/5a7db6c74d55bc51bdf39793
-router.get('/:id', (req, res) => {
-  Job.findById(req.params.id).then((job) => res.json(job));
-});
+router.get('/:id', (req, res, next) => {
+  Job.findById(req.params.id)
+    .then((job) => {
+      if (!job) {
+        res.sendStatus(404)
+      } else {
+        res.json(job)
+      }
+    })
+    .catch(next)
+})
 
 // CREATE
 // POST api/jobs
-router.post('/', (req, res) => {
-  Job.create(req.body).then((job) => res.json(job));
-});
+router.post('/', (req, res, next) => {
+  Job.create(req.body)
+    .then((job) => res.status(201).json(job))
+    .catch(next)
+})
 
 // UPDATE
 // PUT api/jobs/5a7db6c74d55bc51bdf39793
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   Job.findOneAndUpdate({ _id: req.params.id }, req.body, {
-		new: true,
-	}).then((job) => res.json(job));
-});
+    new: true
+  })
+    .then((job) => {
+      if (!job) {
+        res.sendStatus(404)
+      } else {
+        res.json(job)
+      }
+    })
+    .catch(next)
+})
 
 // DESTROY
 // DELETE api/jobs/5a7db6c74d55bc51bdf39793
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   Job.findOneAndDelete({
-		_id: req.params.id,
-	}).then((job) => res.json(job));
-});
-
-module.exports = router;
+    _id: req.params.id
+  })
+    .then((job) => {
+      if (!job) {
+        res.sendStatus(404)
+      } else {
+        res.sendStatus(204)
+      }
+    })
+    .catch(next)
+})
+module.exports = router
--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const jobController = require('./controllers/jobs');
-
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const jobController = require('./controllers/jobs')
 
 // Instantiate express application object
-const app = express();
+const app = express()
 
 // The `.use` method sets up middleware in Express
 
 // Set up cors middleware and make sure that it
 // comes before our routes are used.
-app.use(cors());
+app.use(cors())
 
 // Add `express.json` middleware which will
 // parse JSON requests into JS objects before
 // they reach the route files.
-app.use(express.json());
+app.use(express.json())
 
 // The urlencoded middleware parses requests which use
 // a specific content type (such as when using Axios)
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // Configure the route middleware
-app.use('/api/jobs', jobController);
+app.use('/api/jobs', jobController)
+
+// The last middleware receives any error as its first argument
+app.use((err, req, res, next) => {
+  // If the error contains a statusCode, set the variable to that code
+  // if not, set it to a default 500 code
+  const statusCode = err.statusCode || 500
+  // If the error contains a message, set the variable to that message
+  // if not, set it to a generic 'Internal Server Error'
+  const message = err.message || 'Internal Server Error'
+  // Set the status and send the message as a response to the client
+  res.status(statusCode).send(message)
+})
 
 // Define a port for API to run on, if the environment
 // variable called `PORT` is not found use port 4000
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000)
 // Run server on designated port
 app.listen(app.get('port'), () => {
-  console.log('listening on port ' + app.get('port'));
-});
+  console.log('listening on port ' + app.get('port'))
+})
