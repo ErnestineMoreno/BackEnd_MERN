70b2476181cb636e6bd7d27326b891b713f5e125
kristenmoran <kristen.moran30@gmail.com>
2020-05-15 14:57:30 -0400

    added users and populated docs

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
 // routes/controllers here
 
-router.post('/signup', (req, res, next) => {
-	User.create(req.body)
-		.then((user) => res.status(201).json(user))
-		.catch(next);
+// Using async/await
+// Add the async keyword
+router.post('/signup', async (req, res, next) => {
+	// wrap it in a try/catch to handle errors
+	try {
+		// store the results of any asynchronous calls in variables
+		// and use the await keyword before them
+		const password = await bcrypt.hash(req.body.password, 10);
+		const user = await User.create({ email: req.body.email, password });
+		res.status(201).json(user);
+	} catch (error) {
+		// return the next callback and pass it the error from catch
+		return next(error);
+	}
 });
 
 // SIGN IN
--- models/Job.js
+++ models/Job.js
@@ -7,6 +7,11 @@ const jobSchema = new mongoose.Schema(
 			required: true,
 		},
 		description: String,
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
 	},
 	{
 		timestamps: true,
